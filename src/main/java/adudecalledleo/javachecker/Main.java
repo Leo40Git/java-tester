d067d3b516e9654a70ab98179e48e6d9ebeedffc
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-09-06 22:45:45 +0300

    Add JRE info

--- src/main/java/adudecalledleo/javachecker/Main.java
+++ src/main/java/adudecalledleo/javachecker/Main.java
@@ -8,8 +8,15 @@ public final class Main {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException ignored) { }
 
+        String arch = System.getProperty("os.arch");
+        String version = System.getProperty("java.version");
+        String vendor = System.getProperty("java.vendor");
+
         JOptionPane.showMessageDialog(null,
-                "Seems like your Java Runtime Environment (JRE) is installed correctly!",
+                "Seems like your Java Runtime Environment (JRE) is installed correctly!\n" +
+                        "Architecture: " + arch + "\n" +
+                        "Version: " + version + "\n" +
+                        "Vendor: " + vendor,
                 "Hello from Java!", JOptionPane.INFORMATION_MESSAGE);
     }
 }
